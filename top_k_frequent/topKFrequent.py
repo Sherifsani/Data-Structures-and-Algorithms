2e4650452e6131fb75c4f68bd847cb9960b1592c
Sherif Sani <sanisherif11@gmail.com>
2025-05-29 22:34:09 +0100

    solved top k frequent problem using a third solution

--- top_k_frequent/topKFrequent.py
+++ top_k_frequent/topKFrequent.py
@@ -14,4 +14,22 @@ class Second_Solution:
     def topKFrequent(self, nums:List[int], k:int) -> List[int]:
         counter = Counter(nums)
         return [key[0] for key in list(counter.most_common(k))]
+    
+class Third_Solution:
+    def topKFrequent(self, nums:List[int], k:int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        
+        for num, cnt in count:
+            freq[cnt].append(num)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
         
