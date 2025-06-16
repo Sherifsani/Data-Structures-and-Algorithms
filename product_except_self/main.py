b9972c67f308976175e678904bc5a3df069a32a5
Sherif Sani <sanisherif11@gmail.com>
2025-06-16 08:21:15 +0100

    implement productExceptSelf function to calculate the product of array elements except self

---
+++ product_except_self/main.py
@@ -0,0 +1,21 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        product = 1
+        res = []
+        zero_count = 0
+        for i in nums:
+            if i == 0:  
+                zero_count += 1
+                continue
+            product *= i
+        for i in range(len(nums)):
+            if zero_count == 0:
+                res.append(product // nums[i])
+            elif zero_count == 1:
+                if nums[i] != 0:
+                    res.append(0)
+                    continue
+                res.append(product)
+            else:
+                res.append(0)
+        return res
