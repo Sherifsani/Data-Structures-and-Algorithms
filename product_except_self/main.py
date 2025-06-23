d0c9ecf921c0a2921a46fc19ebedc2b8c7311e7c
Sherif Sani <sanisherif11@gmail.com>
2025-06-23 06:45:01 +0100

    add alternative implementation for productExceptSelf using prefix and postfix arrays

--- product_except_self/main.py
+++ product_except_self/main.py
@@ -19,3 +19,17 @@ class Solution:
             else:
                 res.append(0)
         return res
+
+class Solution_without_divison:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
